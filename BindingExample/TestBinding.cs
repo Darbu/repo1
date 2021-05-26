861b4ef7d41764eb5b9596d18b2cc5b8cfadf336
Darbu <kostas.92@mail.ru>
2021-05-26 06:47:11 +0300

    test commit

---
+++ BindingExample/TestBinding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingExample
+{
+    class TestBinding : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+        private string testProp = "hjk";
+        public string TestProp
+        {
+            get { return testProp; }
+            set
+            {
+                testProp = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("TestProp"));
+            }
+        }
+    }
+
+}
